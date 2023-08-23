d26b1a90df3ee290f5152d509fc7ab3032bfa439
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-08-23 20:05:16 +0600

    example2 updated

--- ObjectOrientedProgramming/Example2.java
+++ ObjectOrientedProgramming/Example2.java
@@ -3,4 +3,25 @@ package ObjectOrientedProgramming;
 public class Example2 {
     // Methods--Are--
     // block-of-codes-that-accept-parameters-and-perform-actions-when-they-are-being-called
+    public void fruit(){
+        String fruit [] = {"Apple","Orange","Banana","Mango"};
+        System.out.println("Fruits: ");
+        for(int i=0; i < fruit.length;i++){
+            System.out.println(fruit[i]);
+        }
+    }
+    public void veges(){
+        String veges [] = {"Potato","Tomato","Cucumber","Carrot"};
+        System.out.println("Vegetables: ");
+        for (int i=0; i< veges.length;i++){
+            System.out.println(veges[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Example2 e = new Example2();
+        e.fruit();
+        e.veges();
+    }
 }
+
