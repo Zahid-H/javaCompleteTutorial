2c5148e8e1f7780f8ca394544c1961d5168bfb7e
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-08-23 19:34:53 +0600

    class and object

---
+++ ObjectOrientedProgramming/ExampleOne.java
@@ -0,0 +1,12 @@
+package ObjectOrientedProgramming;
+
+public class ExampleOne {
+    String name = "Akter Molla";
+    int age = 25;
+
+    public static void main(String[] args) {
+        ExampleOne eo = new ExampleOne();
+        System.out.println("Name: " + eo.name);
+        System.out.println("Age: " + eo.age);
+    }
+}
