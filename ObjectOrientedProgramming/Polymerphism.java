343aafe84ea81aaf63cdd54517ed976d440b3908
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-08-24 20:34:46 +0600

    compile time Polymerphism

---
+++ ObjectOrientedProgramming/Polymerphism.java
@@ -0,0 +1,27 @@
+package ObjectOrientedProgramming;
+
+public class Polymerphism {
+    public String Movie(){
+        return "Movie selection";
+
+    }
+    public String Movie(String collection,String Genre){
+        return collection +" "+ Genre;
+
+    }
+    public String Movie(int time,String date){
+        return time +" "+date;
+    }
+
+    public static void main(String[] args) {
+        Polymerphism p = new Polymerphism();
+        System.out.println(p.Movie());
+        System.out.println(p.Movie("Good colloection","Holloywood"));
+        System.out.println(p.Movie(3,"super sunday"));
+        
+
+
+
+
+    }
+}
