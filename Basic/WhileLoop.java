70e10c52277b8f1900415b21cad93cf68043cd54
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-08-27 22:27:16 +0600

    while loop in java

---
+++ Basic/WhileLoop.java
@@ -0,0 +1,13 @@
+package Basic;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i=0;
+
+        while(i<=10){
+            System.out.println(i+" While is amazing");
+            i++;
+        }
+    }
+
+}
